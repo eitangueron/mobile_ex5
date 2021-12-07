7c0495e51ee4cf7696fb7b77384c829d3292dbe4
Eitan Gueron <eitangueron@gmail.com>
2021-12-07 20:13:09 +0200

    think im done

--- ex5/server.js
+++ ex5/server.js
@@ -2,17 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fs = require('fs');
+const path = require('path')
 // const bodyParser = require('body-parser'); 
 // app.use(bodyParser.json());
 
-const tasks = JSON.parse(fs.readFileSync('./tasks.json', 'utf8'))
+// const tasks = JSON.parse(fs.readFileSync('./tasks.json', 'utf8'))
 
 app.get('/', (req, res) => {
     res.send('Welcome to Eitan\'s server!')
 })
 
-app.get('/tasks', (req, res) => {
-    res.send(tasks)
+app.get('/files/:taskName', (req, res) => {
+    const taskName = req.params.taskName
+    
+    fs.createReadStream(`images/${taskName}.png`).pipe(res)
+    
+    // fs.readFile(`images/${taskName}.png`, (err, data) => {
+    //     if (err) throw err
+    //     res.send(data)
+    // })
 })
 
 app.listen(port, () => {
